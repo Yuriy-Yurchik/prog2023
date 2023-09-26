1fe128720f3c70dc7a4eeecd2b2a50bbac615501
yurii <yyk0205@gmail.com>
2023-09-26 03:04:06 +0300

    Working on booleans.

---
+++ lec3/booleans.py
@@ -0,0 +1,25 @@
+
+# The AND logic function
+print("********** AND **********")
+a, b = False, False
+print("F AND F -- ", a and b)
+a, b = False, True
+print("F AND T -- ", a and b)
+a, b = True, False
+print("T AND F -- ", a and b)
+a, b = True, True
+print("T AND T -- ", a and b)
+
+# The OR logic function
+print("********** OR **********")
+a, b = False, False
+print("F AND F -- ", a or b)
+a, b = False, True
+print("F AND T -- ", a or b)
+a, b = True, False
+print("T AND F -- ", a or b)
+a, b = True, True
+print("T AND T -- ", a or b)
+
+
+
